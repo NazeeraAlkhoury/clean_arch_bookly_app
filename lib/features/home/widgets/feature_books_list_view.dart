a15f721e296c39f7bb4b54c00fbc9ba2db6e651b
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-01-03 15:33:15 +0300

    Create FeatureBooksListView

---
+++ lib/features/home/widgets/feature_books_list_view.dart
@@ -0,0 +1,26 @@
+import 'package:clean_arch_bookly_app/features/home/widgets/custom_book_list_item.dart';
+import 'package:flutter/material.dart';
+
+class FeatureBooksListView extends StatelessWidget {
+  const FeatureBooksListView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Padding(
+        padding: const EdgeInsetsDirectional.only(start: 12),
+        child: SizedBox(
+          height: MediaQuery.sizeOf(context).height * .3,
+          child: ListView.builder(
+            physics: const BouncingScrollPhysics(),
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) => const CustomBookListViewItem(),
+            itemCount: 5,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/home/widgets/home_view_body.dart
+++ lib/features/home/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:clean_arch_bookly_app/features/home/widgets/custom_app_bar.dart';
+import 'package:clean_arch_bookly_app/features/home/widgets/feature_books_list_view.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -9,6 +10,7 @@ class HomeViewBody extends StatelessWidget {
     return const CustomScrollView(
       slivers: [
         CustomAppBar(),
+        FeatureBooksListView(),
       ],
     );
   }
