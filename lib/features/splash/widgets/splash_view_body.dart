ba372bdac1c0a21fd3307bda3402e8a49d0036a1
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-01-03 14:11:14 +0300

    Add SlidingText widget with animation

--- lib/features/splash/widgets/splash_view_body.dart
+++ lib/features/splash/widgets/splash_view_body.dart
@@ -1,10 +1,60 @@
+import 'package:clean_arch_bookly_app/core/utils/app_assets.dart';
+import 'package:clean_arch_bookly_app/features/splash/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late Animation<Offset> slidingAnimation;
+  late AnimationController animationController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    initializeAnimation();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Column();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AppAssets.logo),
+        const SizedBox(
+          height: 4,
+        ),
+        SlidingText(slidingAnimation: slidingAnimation),
+      ],
+    );
+  }
+
+  void initializeAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    final curvedAnimation = CurvedAnimation(
+      parent: animationController,
+      curve: Curves.fastOutSlowIn,
+    );
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 4),
+      end: const Offset(0, 0),
+    ).animate(curvedAnimation);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:clean_arch_bookly_app/core/utils/app_colors.dart';
 import 'package:clean_arch_bookly_app/features/splash/splash_view.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,8 +14,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Clean Architecture Bookly App',
-      theme: ThemeData(),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: AppColors.primaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+      ),
       home: const SplashView(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
