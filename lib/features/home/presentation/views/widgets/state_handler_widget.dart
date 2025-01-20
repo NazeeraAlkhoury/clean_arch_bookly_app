c1c0a38aab9f23492cbd5faa682156928efb93e1
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-01-20 16:45:41 +0300

    create StateHandlerWidget

---
+++ lib/features/home/presentation/views/widgets/state_handler_widget.dart
@@ -0,0 +1,33 @@
+import 'package:clean_arch_bookly_app/core/utils/enums.dart';
+import 'package:flutter/material.dart';
+
+class StateHandlerWidget extends StatelessWidget {
+  final RequestStates state;
+  final WidgetBuilder successWidget;
+  final String? errMessage;
+  const StateHandlerWidget(
+      {super.key,
+      required this.state,
+      required this.successWidget,
+      this.errMessage});
+
+  @override
+  Widget build(BuildContext context) {
+    switch (state) {
+      case RequestStates.successState:
+        return successWidget(context);
+      case RequestStates.failureState:
+        return Center(
+          child: Text(errMessage ?? ''),
+        );
+      case RequestStates.loadingState:
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      case RequestStates.initialState:
+        return Center(
+          child: Text(errMessage ?? ''),
+        );
+    }
+  }
+}
